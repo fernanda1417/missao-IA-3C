bedd2266ae744679f4e50df549ae6703d538193d
fernanda1417 <135229119+fernanda1417@users.noreply.github.com>
2024-10-29 07:57:45 -0300

    ;

---
+++ js/perguntas.js
@@ -0,0 +1,94 @@
+ export const perguntas = [
+    {
+        enunciado: "Você sabe sobre o corpo humano ?",
+        alternativas: [
+            {
+                texto: "Sim",
+                afirmacao: [
+                    "Você é esperto",
+                    ]
+            },
+            {
+                texto: "Não",
+                afirmacao: [
+                    "você poderia aprender mais",
+                
+                    ]
+            }           
+            
+        ];
+        proxima: 1,
+    },
+    {
+        enunciado: "Gostaria de aprender mais sobre?",
+        alternativas: [
+            {
+                texto:"Sim",
+                afirmacao: [
+                    "Você gosta de aprender!",
+                    ]
+            },
+            {
+                texto: "Não",
+                afirmacao: [
+                    "Vocẽ tem a oportunidade, tem certeza?",
+                    ]
+            }
+        ],
+        proxima: 2,
+    },
+    {
+        enunciado: "Quer aprender sobre o sistema digestivo?",
+        alternativas: [
+            {
+                texto:"Sim",
+                afirmacao: [
+                    "Você é determinado, vamos lá",
+                    ]
+            },
+            {
+                texto:"Não",
+                afirmacao: [
+                    "Vocẽ tem a oportunidade, tem certeza?",
+                    ]
+            }
+            
+        ]
+    },
+    proxima: 3,
+    {
+        enunciado: "Vocẽ sabe qual caminho a commida percorre?",
+        alternativas: [
+            {
+                texto:"Sim",
+                afirmacao: [
+                    "Vocẽ é esperto",
+                    ]
+            },
+            {
+                texto: "Não",
+                afirmacao: [
+                    "Vocẽ precisa saber: boca; faringe; esôfago; estomago; intestino delgado; intestino grosso e anus.",
+                    ]
+            }
+        ]
+    },
+    proxima: 4,
+    {
+        enunciado: "Gostaria de aprender mais sobre?",
+        alternativas: [
+            {
+                texto:"Sim",
+                afirmacao: [
+                     "Siga o perfil do tiktok do perfil do professor wesly e aprenda mais.",
+                    ]
+            },
+            {
+                texto: "Não",
+                afirmacao: [
+                    "ok ",
+                    ]
+            }
+        ]
+    },
+];
